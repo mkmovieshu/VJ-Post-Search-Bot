9d95f4f4f662cb0d18fbc8c3a18086cd1c3e189c
mkmovieshu <ipsappdevelopers@gmail.com>
2025-02-24 22:31:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ from os import environ
 API_ID       = int(environ.get("API_ID", "20990520"))
 API_HASH     = environ.get("API_HASH", "714a70d62fc73bf8ec1a5d38adf8f198")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
+DATABASE_URI = environ.get("DATABASE_URI", "7728920673:AAE1NE1D8FeS7V7FN-8bn3tUrRxbmeeXiSw")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002259804858"))
 ADMIN        = int(environ.get("ADMIN", "8185007347"))
 CHANNEL      = environ.get("CHANNEL", "@mkangele")
